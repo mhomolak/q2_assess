e6f2628b8ab57263a2deeb18a04819b4697f593c
Mike Homolak <mhomolak@yahoo.com>
2016-04-07 17:59:34 -0700

    Basic home page setup with links to Books and Authors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile')['development']);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/books', function(req, res, next) {
+  res.render('books');
+});
+
+router.get('/authors', function(req, res, next) {
+  res.render('authors');
 });
 
 module.exports = router;
