1febfd723b132e3d3a4e4b4e8e9f1a76448b4b01
Mike Homolak <mhomolak@yahoo.com>
2016-04-08 17:41:52 -0700

    production heroku

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: 'postgresql://localhost/q2_assessment',
+    connection: 'postgres://nqjtvrhmfifvru:swyrl9mAyAgYbpkDh_9J3kdmHU@ec2-23-23-184-226.compute-1.amazonaws.com:5432/d24rf7u5sq6b0g',
     pool: {
       min: 2,
       max: 10
